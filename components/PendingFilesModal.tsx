1fab757328e7f69dc8c2d8cb0f61c8556f956b5d
wandersondiogenes-creator <wandersondiogenes@gmail.com>
2026-08-24 10:16:20 -0300

    refactor: import useCallback in PendingFilesModal

--- components/PendingFilesModal.tsx
+++ components/PendingFilesModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import { UserProfile } from '@/types/audit';
 import {
   PendingFileRecord,
