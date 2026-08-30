7bd5929e641e0dbfe6424fc7833681b711c8c69d
wandersondiogenes-creator <wandersondiogenes@gmail.com>
2026-08-30 14:39:28 -0300

    feat: add brand identification and UI tabs
    Introduced `getBrandLogo` helper to categorize companies by logo and added state management for new UI tabs (departamentos, comparativo, resumo) in FechamentoView.

---
+++ replace_return2.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const file = 'components/FechamentoView.tsx';
+let code = fs.readFileSync(file, 'utf8');
+
+// I will just use the flat view logic from the previous checkpoint
